6f83f8f7637e1006fc3dd45c2408084fdf90bb3a
Hanno Rein <hanno@hanno-rein.de>
2017-02-20 22:00:34 -0700

    ok

--- match.py
+++ match.py
@@ -3,11 +3,18 @@ import glob
 import difflib
 import html
 
+class Sys(object):
+    def __init__(self,pn,n,lu):
+        self.pn = pn
+        self.n = n
+        self.lu = lu
+
 class Cat(object):
     def __init__(self,path):
         self.path = path
         self.planets = {}
-        self.systems = {}
+        self.systems = []
+        self.lastupdate = {}
         for f in glob.glob(path+"/*.xml"):
             c = ET.parse(f)
             for planet in c.findall(".//planet"):
@@ -17,22 +24,21 @@ class Cat(object):
                     self.planets[name.text] = names[0].text
             names = c.findall("./name")
             names_text = []
+            try:
+                lu = c.find(".//lastupdate").text
+            except:
+                lu = ""
             for name in names:
-                self.systems[name.text] = names[0].text
+                self.systems.append(Sys(names[0].text,name.text,lu))
     def findnews(self,other):
-        usnames = list(set(self.systems.values()))
+        usnames = list(set([s.pn for s in self.systems]))
+        othernames = [s.n for s in other.systems]
         snew = []
         for usname in usnames:
-            if usname not in other.systems:
-                snew.append([usname,difflib.get_close_matches(usname,other.systems.keys())])
+            if usname not in othernames:
+                snew.append([usname,difflib.get_close_matches(usname,othernames)])
+
         return snew
-    def findnew(self,other):
-        unames = list(set(self.planets.values()))
-        new = []
-        for uname in unames:
-            if uname not in other.planets:
-                new.append([uname,difflib.get_close_matches(uname,other.planets.keys())])
-        return new
     def getsystem(self,systemname):
         with open(self.path+"/"+systemname+".xml","r") as f:
             return f.read()
@@ -44,20 +50,33 @@ eeu = Cat("systems_exoplaneteu")
 
 news = eeu.findnews(oec)
 
+from flask import Flask, request
 
-o = "<html><body>"
-o += """ <form action=""> """
-for i,n in enumerate(news):
-    pn, ons = n
-    o+="<h2>%03d/%03d &nbsp;&nbsp;&nbsp; "%(i+1,len(news))+pn+"</h2>"
-    o+="<input type='radio' name='s%05d' value='ignore' checked>ignore<br/>\n" %i
-    for j,on in enumerate(ons):
-        o+="<input type='radio' name='s%05d' value='on%02d'>add name to oec:  %s<br/>\n" %(i,j,on)
+app = Flask(__name__)
 
-    o+="<input type='radio' name='s%05d' value='addsystem'>add system to oec<br/>\n" %i
-    o+="<pre style='margin-left: 3em;'>"+html.escape(eeu.getsystem(pn))+"</pre>\n"
-o += "</form></body></html>"
-    
+@app.route("/")
+def hello():
+    o = "<html><body>"
+    o += """ <form action="./update"  method="post">   <input type="submit" value="Submit"> """
+    for i,n in enumerate(news):
+        pn, ons = n
+        o+="<h2>%03d/%03d &nbsp;&nbsp;&nbsp; "%(i+1,len(news))+pn+"</h2>"
+        o+="<input type='radio' name='s%05d' value='ignore' checked>ignore<br/>\n" %i
+        for j,on in enumerate(ons):
+            o+="<input type='radio' name='s%05d' value='on%02d'>add name to oec:  %s<br/>\n" %(i,j,on)
+
+        o+="<input type='radio' name='s%05d' value='addsystem'>add system to oec<br/>\n" %i
+        o+="<pre style='margin-left: 3em;'>"+html.escape(eeu.getsystem(pn))+"</pre>\n"
+    o += "</form></body></html>"
+    return o
 
 
-print(o)
+@app.route('/update', methods=['POST'])
+def login():
+    o = ""
+    for i,n in enumerate(news):
+        o+= "%d  "%i + request.form['s%05d'%i] + "<br/>"
+    return o
+
+if __name__ == "__main__":
+    app.run()
